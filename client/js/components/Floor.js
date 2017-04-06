734a1fa7e9f30a194588901520b67c4d007a0cb2
Rudy Perez <rudy@mediaartslab.com>
2017-04-05 17:34:51 -0700

    mods for labyrinth

--- client/js/components/Floor.js
+++ client/js/components/Floor.js
@@ -27,14 +27,14 @@ export default class Floor extends Component {
 	}
 
 	tiltboard(rot){
-		console.log('tiltboard function... ', rot);
+		//console.log('tiltboard function... ', rot);
 		var xtilt = rot.xaxis;
 		var ytilt = rot.yaxis;
 		var ztilt = rot.zaxis;
 
 
 		this.setState({
-			rotation: xtilt + " " + ytilt + " " + 0
+			rotation: xtilt + " " + 0 + " " + ytilt
 		})
 	}
 
@@ -48,15 +48,17 @@ export default class Floor extends Component {
 
 	render(){
 
-		console.log(this.state.rotation)
+		//console.log(this.state.rotation)
 
 
 		return (
 
 			<a-scene vr-mode-ui="true" physics="friction: 0.1; restitution: 0.5; gravity:-100">
 
-				<a-light type="hemisphere" color="#222" intensity="2" position="0 20 0"></a-light>
-				<a-sphere dynamic-body="mass:10000" position="1.29 26 1.03" scale="1 1 1" color="yellow"></a-sphere>
+				
+				<a-entity light="type: hemisphere; color: #929292; groundColor: #515151; intensity: 1.8" position="0 20 0"></a-entity>
+
+				<a-sphere dynamic-body="mass:10000" position="-6.29 26 1.03" scale="1 1 1" color="yellow"></a-sphere>
 
 				<a-asset-item id="model" src={Modelpath}></a-asset-item>
 				<a-asset-item id="modmlt" src={Matpath}></a-asset-item>
@@ -65,10 +67,10 @@ export default class Floor extends Component {
 
 				
 
-				<a-entity camera position="2 14 18" rotation="0 0 0" wasd-controls look-controls></a-entity>
+				<a-entity camera position="-0.2 34 28" rotation="-53 0 0" wasd-controls look-controls></a-entity>
 
 
-				<a-plane static-body color="#666" height="100" width="100" rotation="-90 0 0"></a-plane>
+				
 				<a-sky color="#ffffff"></a-sky>
 
 			</a-scene>
